6702dfc0141c89ff17ee24b17a1c4246cbbda37f
Armando Machado <armandomachado13@yahoo.com>
2022-11-21 23:58:55 -0300

    V.2

--- server.js
+++ server.js
@@ -1,32 +1,52 @@
 const express = require('express');
+const { Server: SocketServer } = require('socket.io');
+const { Server: HttpServer } = require('http');
+
 const app = express();
 
+const PORT = 8080;
+
+const httpServer = new HttpServer(app);
+
+const io = new SocketServer(httpServer);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-app.set('view engine', 'ejs');
-
 const productList = [];
+const mensajes = [];
 
-app.get('/', (req, res) => {
+app.use(express.static('public'));
 
-  res.render('pages/index');
+io.on('connection', (socket) => {
+
+  socket.emit('products', productList);
+
+  socket.on('newProduct', (newProduct) => {
+    
+    productList.push(newProduct);
+    io.sockets.emit('products', productList);
+
+  });
+
+  socket.emit('mensajes', mensajes);
+
+  socket.on('newMensaje', (newMensaje) => {
+    
+    mensajes.push(newMensaje);
+    io.sockets.emit('mensajes', mensajes);
+
+  });
 });
 
-app.post('/productos', (req,res) =>{
-    productList.push(req.body);
-    console.log(productList);
-    res.redirect('/');
+const connectedServer = httpServer.listen(PORT, () => {
+  console.log('RUNNING...');
+  console.log(`Servidor Http con Websockets escuchando en el puerto ${connectedServer.address().port}`)
 });
 
-app.get('/productos', (req,res)=>{
-    res.render('pages/productos', {productList});
-})
 
 
 
 
-const PORT = 8080;
-app.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`));
+
+
